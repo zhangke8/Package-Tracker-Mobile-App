05478ac7a134002a5960cb1b25b2959ad5daa591
Kevin Zhang <19340295+zhangke8@users.noreply.github.com>
2018-04-26 09:03:51 -0400

    formatting

--- PackageTracker/src/Generate Static Data.js
+++ PackageTracker/src/Generate Static Data.js
@@ -20,37 +20,37 @@ var OrderSchema = new Schema({
 }, { collection: 'orders' });
 
 var Order = mongoose.model('orders', OrderSchema, 'orders');
- var products = [
- Order({
-		orderName:"computer", 
-		price:900.00, 
-		trackerID:489712165498461165, 
-		imageUrl:"https://pisces.bbystatic.com/image2/BestBuy_US/store/ee/2015/com/pm/nav_desktops_1115.jpg"	
-	}),
-Order({
-		orderName:"tablet", 
-		price:300.00, 
-		trackerID:789456232619849165, 
-		imageUrl:"https://gloimg.gbtcdn.com/gb/pdm-product-pic/Electronic/2018/01/31/goods-img/1517356503798430531.jpg"
-	}), 
-Order({
-		orderName:"Ramen", 
-		price:20.00,  
-		trackerID:1321187986526548798, 
-		imageUrl:"https://images-na.ssl-images-amazon.com/images/I/51PXoeVnMWL.jpg"
-	}),
-item4 = new Order({
-		orderName:"Shampoo", 
-		price:10.00, 
-		trackerID:62625198426165, 
-		imageUrl:"https://www.dollartree.com/assets/product_images_2016/styles/xlarge/433474.jpg"
-	}),
-Order({
-		orderName:"Car", 
-		price:40000.00, 
-		trackerID:784982062984984654, 
-		imageUrl:"https://article.images.consumerreports.org/prod/content/dam/CRO%20Images%202018/Magazine/04April/CRM-Cars-inline-TopTen-Toyota-Corolla-2018-4-18"
-	})
+var products = [
+	 Order({
+			orderName:"computer", 
+			price:900.00, 
+			trackerID:489712165498461165, 
+			imageUrl:"https://pisces.bbystatic.com/image2/BestBuy_US/store/ee/2015/com/pm/nav_desktops_1115.jpg"	
+		}),
+	Order({
+			orderName:"tablet", 
+			price:300.00, 
+			trackerID:789456232619849165, 
+			imageUrl:"https://gloimg.gbtcdn.com/gb/pdm-product-pic/Electronic/2018/01/31/goods-img/1517356503798430531.jpg"
+		}), 
+	Order({
+			orderName:"Ramen", 
+			price:20.00,  
+			trackerID:1321187986526548798, 
+			imageUrl:"https://images-na.ssl-images-amazon.com/images/I/51PXoeVnMWL.jpg"
+		}),
+	item4 = new Order({
+			orderName:"Shampoo", 
+			price:10.00, 
+			trackerID:62625198426165, 
+			imageUrl:"https://www.dollartree.com/assets/product_images_2016/styles/xlarge/433474.jpg"
+		}),
+	Order({
+			orderName:"Car", 
+			price:40000.00, 
+			trackerID:784982062984984654, 
+			imageUrl:"https://article.images.consumerreports.org/prod/content/dam/CRO%20Images%202018/Magazine/04April/CRM-Cars-inline-TopTen-Toyota-Corolla-2018-4-18"
+		})
  ];
 mongoose.connect("mongodb://localhost:27017/PackageTracking", function (err, response) {
 	if (err) { console.log(err); }
