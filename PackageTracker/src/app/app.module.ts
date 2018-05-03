9d729f099f7b60ebcd8e5037d98d97a9edce0c44
Kevin Zhang <19340295+zhangke8@users.noreply.github.com>
2018-05-03 17:05:47 -0400

    add in product list

--- PackageTracker/src/app/app.module.ts
+++ PackageTracker/src/app/app.module.ts
@@ -8,13 +8,16 @@ import { CreateOrderComponent } from './orders/create-order.component';
 import { HttpModule } from '@angular/http';
 import { OrderService } from './orders/order.service';
 import { ItemService } from './items/item.service';
+import { ProductListComponent } from './products/product-list.component';
 import { OrderDetailComponent } from './orders/order-detail.component';
+import { ProductApiService } from './products/product.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateOrderComponent,
-    OrderDetailComponent
+    OrderDetailComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
@@ -24,11 +27,13 @@ import { OrderDetailComponent } from './orders/order-detail.component';
     RouterModule.forRoot([
       { path: 'createorder', component: CreateOrderComponent},
       { path: 'orderdetail/:id', component: OrderDetailComponent},
+      { path: 'products', component: ProductListComponent},
       { path: '', redirectTo: 'createorder', pathMatch: 'full'},
       { path: '**', redirectTo: 'createorder', pathMatch: 'full'}
     ])
   ],
-  providers: [OrderService, ItemService],
+  // providers: [OrderService, ItemService],
+  providers: [OrderService, ItemService, ProductApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
