fe301f7e56f977bc9bdfd2e36a2ae0460245517e
Kevin Zhang <19340295+zhangke8@users.noreply.github.com>
2018-05-09 16:35:54 -0400

    clear out

--- TrackerApp - E/OrderTracking/ordertracking/routes/api/orders.route.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-
-var router = express.Router()
-
-var OrderController = require('../../controllers/orders.controller');
-
-router.get('/',OrderController.getOrders)
-
-module.exports = router;
