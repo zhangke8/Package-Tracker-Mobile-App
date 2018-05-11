081fb72fbf2cfb19451f0fea41f233d2c4ed5d0c
Kevin Zhang <19340295+zhangke8@users.noreply.github.com>
2018-05-11 11:19:40 -0400

    integrating more of tracker

--- PackageTracker/src/app/orders/order-detail.component.ts
+++ PackageTracker/src/app/orders/order-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { OrderService } from './order.service';
 import { IItem } from '../items/item';
 import { ActivatedRoute, Router} from '@angular/router';
-// import { IOrder } from './order';
+import { IOrder } from './order';
 // import { ITrackingStatus } from './trackingstatus';
 // import { TrackingService } from './tracking.service';
 // import { ITrackingLocations } from './trackinglocations';
@@ -13,10 +13,10 @@ import { ActivatedRoute, Router} from '@angular/router';
   styleUrls: ['./order-detail.component.css']
 })
 export class OrderDetailComponent implements OnInit {
-  // order: IOrder;
+  order: IOrder;
   // trackstatus: ITrackingStatus;
   // tracklocations: ITrackingLocations[];
-  // errorMessage: string;
+  errorMessage: string;
   constructor(private route: ActivatedRoute,
     private router: Router,
     private orderService: OrderService) { }
