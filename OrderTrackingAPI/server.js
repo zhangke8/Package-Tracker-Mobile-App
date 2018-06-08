d5f13ebaff7db4d01960651d45eba5c234946c23
2gs <31971265+2gs@users.noreply.github.com>
2018-06-08 12:00:15 -0400

    Updated server.js

--- OrderTrackingAPI/server.js
+++ OrderTrackingAPI/server.js
@@ -12,35 +12,49 @@ require('./config/passport.js');
 const orderRoute = require('./app/routes/orderRoute');
 const itemRoute = require('./app/routes/itemRoute');
 const authRoute = require('./app/routes/authenticationRoute');
-
-const db1 = mongoose.connect("mongodb://localhost:27017/PackageTracking", function (err, response) {
-  if (err) { console.log(err); }
-  else { console.log('Connected to ' + db1, ' + ', response); }
-});
-
 const app = express();
+
+//Port
+const port = 8000;
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*'); //change to *
+  res.setHeader('Access-Control-Allow-Methods', '*'); //change to*
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 morganBody(app);
 
 app.use(logger('dev'));
-
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-  });
-
 app.use(passport.initialize());
-app.get('/', (req, res)=>{res.send("hey!")});
 app.use('/api',orderRoute);
 app.use('/api',itemRoute);
 app.use('/api',authRoute);
 
-const port = 8000;
+//localhost:27017
+const db1 = mongoose.connect("mongodb://database/Package-Tracker-Mobile-App-master", function (err, response) {
+  if (err) { console.log(err); }
+  else { console.log('Connected to ' + db1, ' + ', response); }
+});
+
+
+
+//move up
+/*
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200'); //change to *
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); //change to*
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
+*/
+
+
 
 app.listen(port, function () {
     console.log('Example app listening on port 8000!');
